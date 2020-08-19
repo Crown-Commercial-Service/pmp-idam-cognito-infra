8086027bf49961cf11c997349c77682b8ccd044c
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-08-19 14:05:26 +0100

    versions changes

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@
 #  Configure the AWS Provider                                                  #
 ## =============================================================================
 
-terraform {
-  required_version = ">=0.12"
-  required_providers {
-    aws = "~> 3.0"
-  }
-}
+# terraform {
+#   required_version = ">=0.12"
+#   required_providers {
+#     aws = "~> 3.0"
+#   }
+# }
 
 provider "aws" {
   region  = var.region
