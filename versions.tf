f0676c07a9b5a6e1cfb0529f70dadb54dfa06026
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-10-07 16:49:06 +0100

    s3 backend simpify

--- versions.tf
+++ versions.tf
@@ -5,8 +5,22 @@ terraform {
     aws = ">= 3.0"
   }
 
-  backend "s3" {
-    bucket         = "pmp-terraform-state-${var.environment}"
+
+  backend "s3" {}
+
+#   backend "s3" {
+#     bucket         = "pmp-terraform-state-${environment}"
+#     key            = "ccs-pmp-infra-idam"
+#     region         = "eu-west-2"
+#     dynamodb_table = "pmp_terraform_state_lock-${environment}"
+#     encrypt        = true
+#   }
+}
+
+  data "terraform_remote_state" "state" {
+  backend = "s3"
+  config {
+   bucket         = "pmp-terraform-state-${var.environment}"
     key            = "ccs-pmp-infra-idam"
     region         = "eu-west-2"
     dynamodb_table = "pmp_terraform_state_lock-${var.environment}"
