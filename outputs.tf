87d6a96302d4e67291e13dc4b919f2736a49a33c
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-10-08 15:04:56 +0100

    refactor

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,3 @@
-output "id" {
-  description = "The id of the user pool"
-  value       = aws_cognito_user_pool.user_pool.id
-  senstive = true
-}
-
-output "arn" {
-  description = "The ARN of the user pool"
-  value       = aws_cognito_user_pool.user_pool.arn
-  senstive = true
-}
-
-output "endpoint" {
-  description = "The endpoint name of the user pool. Example format: cognito-idp.REGION.amazonaws.com/xxxx_yyyyy"
-  value       = aws_cognito_user_pool.user_pool.endpoint
-  senstive = true;
-}
-
 output "creation_date" {
   description = "The date the user pool was created"
   value       = aws_cognito_user_pool.user_pool.creation_date
