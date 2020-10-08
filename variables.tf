fbfc3d5c6193e00f66e9c7c7160ea7b2727c1a14
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-10-08 15:17:23 +0100

    account removed

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@
 data "aws_caller_identity" "current" {}
 
 output "aws_account_id" {
-  value = "${data.aws_caller_identity.current.account_id}"
+  # value = "${data.aws_caller_identity.current.account_id}"
 }
 
 variable "env_var" {
