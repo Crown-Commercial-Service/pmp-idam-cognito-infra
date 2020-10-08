8483e89ae09c2491d6db6244b94a6b542a57bdca
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-10-08 10:15:41 +0100

    version updated

--- versions.tf
+++ versions.tf
@@ -4,15 +4,4 @@ terraform {
   required_providers {
     aws = ">= 3.0"
   }
-
-
-  # backend "s3" {}
-
-  # backend "s3" {
-  #   bucket         = "pmp-terraform-state-${environment}"
-  #   key            = "ccs-pmp-infra-idam"
-  #   region         = "eu-west-2"
-  #   dynamodb_table = "pmp_terraform_state_lock-${environment}"
-  #   encrypt        = true
-  # }
 }
