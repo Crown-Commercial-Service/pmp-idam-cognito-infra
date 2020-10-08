7fb1e3fafb1b508b97f2bd6ec6911a427618add7
“Naresh <naresh.kumar@crowncommercial.gov.uk>
2020-10-08 09:00:57 +0100

    backend config changes

--- versions.tf
+++ versions.tf
@@ -6,15 +6,15 @@ terraform {
   }
 
 
-  //backend "s3" {}
+  backend "s3" {}
 
-  backend "s3" {
-    bucket         = "pmp-terraform-state-${environment}"
-    key            = "ccs-pmp-infra-idam"
-    region         = "eu-west-2"
-    dynamodb_table = "pmp_terraform_state_lock-${environment}"
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "pmp-terraform-state-${environment}"
+  #   key            = "ccs-pmp-infra-idam"
+  #   region         = "eu-west-2"
+  #   dynamodb_table = "pmp_terraform_state_lock-${environment}"
+  #   encrypt        = true
+  # }
 }
 
   # data "terraform_remote_state" "state" {
